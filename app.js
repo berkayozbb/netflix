edd772a870d9de7d3ee09f235e5a8b192d835e34
Berkay <116227509+berkayozbb@users.noreply.github.com>
2023-02-06 22:24:45 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-$("login.html").ready(function () {
-    alert("Anasayfaya dönmek için Netflix logosuna, Kim İzliyor ekranına geçiş yapmak için Kullanıcı adı: berkay Şifre: berkay  olacak şekilde formu doldurup Oturum Aç butonuna tıklayınız.")
-});
+//$("login.html").ready(function () {
+//    alert("Anasayfaya dönmek için Netflix logosuna, Kim İzliyor ekranına geçiş yapmak için Kullanıcı adı: berkay Şifre: berkay  olacak şekilde formu doldurup Oturum Aç butonuna tıklayınız.")
+//});
 
 $("#giris").click(function () { 
     if (username.value == "berkay" && password.value == "berkay"){
